557b613a2a3feed0d41b1e31728c9db923aa149d
Ariel <Ariel.Salem1989@gmail.com>
2017-05-02 09:09:58 -0700

    Now with redux

--- src/routes/register/Register.js
+++ src/routes/register/Register.js
@@ -2,7 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './Register.css';
-import Messages from '../Messages';
 
 class Register extends React.Component {
   constructor(props) {
@@ -11,32 +10,28 @@ class Register extends React.Component {
   }
   static propTypes = {
     title: PropTypes.string.isRequired,
-    // NEED TO FIGURE OUT HOW TO INCORPORATE
-    // name: PropTypes.string.isRequired,
-    // email: PropTypes.string.isRequired,
-    // password: PropTypes.string.isRequired
   };
 
-  handleChange(event) {
-    this.setState({ [event.target.name]: event.target.value });
-  }
+  // handleChange(event) {
+  //   this.setState({ [event.target.name]: event.target.value });
+  // }
 
-  handleSignup(event) {
-    event.preventDefault();
-    this.props.dispatch(signup(this.state.name, this.state.email, this.state.password));
-  }
+  // handleSignup(event) {
+  //   event.preventDefault();
+  //   this.props.dispatch(signup(this.state.name, this.state.email, this.state.password));
+  // }
 
-  handleFacebook() {
-    this.props.dispatch(facebookLogin())
-  }
+  // handleFacebook() {
+  //   this.props.dispatch(facebookLogin())
+  // }
 
-  handleTwitter() {
-    this.props.dispatch(twitterLogin())
-  }
+  // handleTwitter() {
+  //   this.props.dispatch(twitterLogin())
+  // }
 
-  handleGoogle() {
-    this.props.dispatch(googleLogin())
-  }
+  // handleGoogle() {
+  //   this.props.dispatch(googleLogin())
+  // }
 
 
   render() {
@@ -45,34 +40,116 @@ class Register extends React.Component {
       <div className={s.root}>
         <div className={s.container}>
           <h1>{this.props.title}</h1>
-            <div className="login-container">
-              <div className="panel">
-                <div className="panel-body">
-                  <Messages messages={this.props.messages}/>
-                  <form /*onSubmit={this.handleSignup.bind(this)}*/>
-                    <legend>Create an account</legend>
-                    <div className="form-group">
-                      <label htmlFor="name">Name</label>
-                      <input type="text" name="name" id="name" placeholder="Name" autoFocus className="form-control" value={this.state.name} /*onChange={this.handleChange.bind(this)}*//>
-                    </div>
-                    <div className="form-group">
-                      <label htmlFor="email">Email</label>
-                      <input type="email" name="email" id="email" placeholder="Email" className="form-control" value={this.state.email} /*onChange={this.handleChange.bind(this)}*//>
-                    </div>
-                    <div className="form-group">
-                      <label htmlFor="password">Password</label>
-                      <input type="password" name="password" id="password" placeholder="Password" className="form-control" value={this.state.password} /*onChange={this.handleChange.bind(this)}*//>
-                    </div>
-                    <div className="form-group">
-                      <small className="text-muted"> By signing up, you agree to the <a href='/login'>Terms of Service </a>.</small>
-                    </div>
-                    <button type="submit" className="btn btn-success">Create an account</button>
-                  </form>
-                  <div className="hr-title"><span>or</span></div>
-                  <div className="btn-toolbar text-center">
-                    <button /*onClick={this.handleFacebook.bind(this)}*/ className="btn btn-facebook">Sign in with Facebook</button>
-                    <button /*onClick={this.handleTwitter.bind(this)}*/ className="btn btn-twitter">Sign in with Twitter</button>
-                    <button /*onClick={this.handleGoogle.bind(this)}*/ className="btn btn-google">Sign in with Google</button>
+            <div className="panel">
+              <div className="panel-body">
+                <strong className={s.lineThrough}>Create an Account</strong>
+                <form method="post"/*onSubmit={this.handleSignup.bind(this)}*/>
+                  <div className={s.formGroup}>
+                    <label className={s.label} htmlFor="name">
+                      Name:
+                    </label>
+                    <input
+                      className={s.input}
+                      id="name"
+                      type="text"
+                      name="name"
+                      autoFocus
+                       /*onChange={this.handleChange.bind(this)}*/
+                       />
+                  </div>
+                  <div className={s.formGroup}>
+                    <label className={s.label} htmlFor="usernameOrEmail">
+                      Username or email address:
+                    </label>
+                    <input
+                      className={s.input}
+                      id="usernameOrEmail"
+                      type="text"
+                      name="usernameOrEmail"
+                      autoFocus
+                       /*onChange={this.handleChange.bind(this)}*/
+                       />
+                  </div>
+                  <div className={s.formGroup}>
+                    <label className={s.label} htmlFor="password">
+                      Password:
+                    </label>
+                   <input
+                     className={s.input}
+                     id="password"
+                     type="text"
+                     name="password"
+                     autoFocus
+                      /*onChange={this.handleChange.bind(this)}*/
+                      />
+                  </div>
+                  <div className="form-group">
+                    <small className="text-muted"> By signing up, you agree to the <a href='/login'>Terms of Service </a>.</small>
+                  </div>
+                  <button type="submit" className="btn btn-success">Create an account</button>
+                </form>
+                <strong className={s.lineThrough}>OR</strong>
+                <div className="btn-toolbar text-center">
+                  <div className={s.formGroup}>
+                    <a className={s.facebook} /*onClick={this.handleFacebook.bind(this)}*/ href="/login/facebook">
+                      <svg
+                        className={s.icon}
+                        width="30"
+                        height="30"
+                        viewBox="0 0 30 30"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          d="M22 16l1-5h-5V7c0-1.544.784-2 3-2h2V0h-4c-4.072 0-7 2.435-7 7v4H7v5h5v14h6V16h4z"
+                        />
+                      </svg>
+                      <span>Login with Facebook</span>
+                    </a>
+                  </div>
+                  <div className={s.formGroup}>
+                    <a className={s.google} /*onClick={this.handleGoogle.bind(this)}*/href="/login/google">
+                      <svg
+                        className={s.icon}
+                        width="30"
+                        height="30"
+                        viewBox="0 0 30 30"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          d={'M30 13h-4V9h-2v4h-4v2h4v4h2v-4h4m-15 2s-2-1.15-2-2c0 0-.5-1.828 1-3 ' +
+                          '1.537-1.2 3-3.035 3-5 0-2.336-1.046-5-3-6h3l2.387-1H10C5.835 0 2 3.345 2 7c0 ' +
+                          '3.735 2.85 6.56 7.086 6.56.295 0 .58-.006.86-.025-.273.526-.47 1.12-.47 1.735 ' +
+                          '0 1.037.817 2.042 1.523 2.73H9c-5.16 0-9 2.593-9 6 0 3.355 4.87 6 10.03 6 5.882 ' +
+                          '0 9.97-3 9.97-7 0-2.69-2.545-4.264-5-6zm-4-4c-2.395 0-5.587-2.857-6-6C4.587 ' +
+                          '3.856 6.607.93 9 1c2.394.07 4.603 2.908 5.017 6.052C14.43 10.195 13 13 11 ' +
+                          '13zm-1 15c-3.566 0-7-1.29-7-4 0-2.658 3.434-5.038 7-5 .832.01 2 0 2 0 1 0 ' +
+                          '2.88.88 4 2 1 1 1 2.674 1 3 0 3-1.986 4-7 4z'}
+                        />
+                      </svg>
+                      <span>Login with Google</span>
+                    </a>
+                  </div>
+                  <div className={s.formGroup}>
+                    <a className={s.twitter} /*onClick={this.handleTwitter.bind(this)}*/ href="/login/twitter">
+                      <svg
+                        className={s.icon}
+                        width="30"
+                        height="30"
+                        viewBox="0 0 30 30"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          d={'M30 6.708c-1.105.49-2.756 1.143-4 1.292 1.273-.762 2.54-2.56 ' +
+                          '3-4-.97.577-2.087 1.355-3.227 1.773L25 5c-1.12-1.197-2.23-2-4-2-3.398 0-6 ' +
+                          '2.602-6 6 0 .4.047.7.11.956L15 10C9 10 5.034 8.724 2 5c-.53.908-1 1.872-1 ' +
+                          '3 0 2.136 1.348 3.894 3 5-1.01-.033-2.17-.542-3-1 0 2.98 4.186 6.432 7 7-1 ' +
+                          '1-4.623.074-5 0 .784 2.447 3.31 3.95 6 4-2.105 1.648-4.647 2.51-7.53 2.51-.5 ' +
+                          '0-.988-.03-1.47-.084C2.723 27.17 6.523 28 10 28c11.322 0 17-8.867 17-17 ' +
+                          '0-.268.008-.736 0-1 1.2-.868 2.172-2.058 3-3.292z'}
+                        />
+                      </svg>
+                      <span>Login with Twitter</span>
+                    </a>
                   </div>
                 </div>
               </div>
